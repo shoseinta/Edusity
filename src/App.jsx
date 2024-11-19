2ce032e1a98ed0f4050bfcf13f7268854767e34e
shoseinta <htaheri.76@gmail.com>
2024-11-19 17:13:50 +0330

    decomposition of Campus

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
---
+++ src/Features/Campus/Gallery.jsx
@@ -0,0 +1,17 @@
+import gallery_1 from "../../assets/gallery-1.png";
+import gallery_2 from "../../assets/gallery-2.png";
+import gallery_3 from "../../assets/gallery-3.png";
+import gallery_4 from "../../assets/gallery-4.png";
+
+const Gallery = () => {
+    return(
+      <div className="gallery">
+        <img src={gallery_1} alt="" />
+        <img src={gallery_2} alt="" />
+        <img src={gallery_3} alt="" />
+        <img src={gallery_4} alt="" />
+      </div>
+    )
+}
+
+export default Gallery;
