c6b2013a92947cd34ab9e29b5f52d847d4bce189
shoseinta <htaheri.76@gmail.com>
2024-11-20 13:09:16 +0330

    constants modified

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,13 +1,13 @@
-import program_1 from "../../../assets/program-1.png";
-import program_2 from "../../../assets/program-2.png";
-import program_3 from "../../../assets/program-3.png";
-import program_icon_1 from "../../../assets/program-icon-1.png";
-import program_icon_2 from "../../../assets/program-icon-2.png";
-import program_icon_3 from "../../../assets/program-icon-3.png";
-import user_1 from "../../../assets/user-1.png";
-import user_2 from "../../../assets/user-2.png";
-import user_3 from "../../../assets/user-3.png";
-import user_4 from "../../../assets/user-4.png";
+import program_1 from "../assets/program-1.png";
+import program_2 from "../assets/program-2.png";
+import program_3 from "../assets/program-3.png";
+import program_icon_1 from "../assets/program-icon-1.png";
+import program_icon_2 from "../assets/program-icon-2.png";
+import program_icon_3 from "../assets/program-icon-3.png";
+import user_1 from "../assets/user-1.png";
+import user_2 from "../assets/user-2.png";
+import user_3 from "../assets/user-3.png";
+import user_4 from "../assets/user-4.png";
 
 export const inputsData = [
     {
