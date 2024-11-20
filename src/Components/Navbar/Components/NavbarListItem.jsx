df8c03b8d7e97e5fca61a58c37a471937d5894bc
shoseinta <htaheri.76@gmail.com>
2024-11-20 10:29:28 +0330

    Navbar css

--- src/Components/Navbar/Components/NavbarListItem.jsx
+++ src/Components/Navbar/Components/NavbarListItem.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-scroll";
+import './NavbarListItem.css';
 
 const NavbarListItem = ({ linkTo, offset, label, smooth, duration }) => {
   return (
