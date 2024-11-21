6f1db25247731d7e91197a01fea7a04d7dd494fa
shoseinta <htaheri.76@gmail.com>
2024-11-21 07:38:56 +0330

    centralized paragraphs data

---
+++ src/Constants/aboutParagraphs.js
@@ -0,0 +1,22 @@
+export const AboutParagraphs = [
+    {
+      id: 1,
+      content: `Embark on a transformative educational journey with our university's
+          comprehensive education programs. Our cutting-edge curriculum is
+          designed to empower students with the knowledge, skills, and
+          experinces needed to excel in the dynamic field of education.`,
+    },
+    {
+      id: 2,
+      content: `With a focus on innovation, hands-on learning, and personalize
+          mentorship, our pograms prepare aspiring educators to make a
+          meaningful impact in classrooms, schools, and communities.`,
+    },
+    {
+      id: 3,
+      content: `Whether you aspire to become a teacher, administrator, counselor, or
+          educational leader, our diverse range of programs offers the perfect
+          pathway to achieve your goals and unlock your full potential in
+          shaping the future of education.`,
+    },
+  ];
